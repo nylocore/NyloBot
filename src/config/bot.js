ba42c676bb097c8700a645ddf4203bbfe2777738
nylocore <thenylofox@outlook.com>
2026-09-17 23:42:37 -0500

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -23,8 +23,8 @@ export const botConfig = {
     // 5 = Competing
     activities: [
       {
-        name: "Custom Status", // required by Discord API, not shown in the client
-        state: "Watching",     // this is what people actually see
+        name: "Watching", // required by Discord API, not shown in the client
+        state: "‎ ",     // this is what people actually see
         type: 3,               // Custom
       },
     ],
